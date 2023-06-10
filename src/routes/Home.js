75738535a50e4e4f3cedaea3d2a1fbf9582adde4
larsyaeger <larsyaeger@gmail.com>
2023-06-10 11:44:07 -0700

    minor change

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default home;
