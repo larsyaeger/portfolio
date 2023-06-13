c96664e165ba955b610d18c2cf6506763cb69170
larsyaeger <larsyaeger@gmail.com>
2023-06-13 11:23:12 -0700

    working on hamburger icon and some logic for hiding and displaying navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import "./NavbarStyles.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="header">
       <Link to="/">
         <h1>My portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -23,8 +25,12 @@ const Navbar = () => {
           <Link to="/Contact">Contact</Link>
         </li>
       </ul>
-      <div className="hamburger">
-        <FaBars size={20} style={{ color: "#fff" }} />
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
       </div>
     </div>
   );
