e3f4e2b05fdddbcfcbfda37628953304463c4531
larsyaeger <larsyaeger@gmail.com>
2023-06-14 13:02:09 -0700

    import heroImg

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import HeroImg from "../components/HeroImg";
 
 const home = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg />
     </div>
   );
 };
