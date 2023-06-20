f64f68eb3f30d6af240edf8bb191e60fe3d5132f
larsyaeger <larsyaeger@gmail.com>
2023-06-20 10:59:01 -0700

    further building out the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "./FooterStyles.css";
 
 import React from "react";
 
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaPhone, FaMailBulk, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -11,9 +11,43 @@ const Footer = () => {
         <div className="left">
           <div className="location">
             <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>address goes here</p>
+              <p>city goes here</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              123456789
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              larsyaeger@gmail.com
+            </h4>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <h4>About the company</h4>
+          <p>
+            ladsfasfsadf adsfg kadfsjg kldfjs gkl jaersig j9aerigju 89aw4fg
+            89ahjf jkasj fklas kfjhas jkfajksf
+          </p>
+          <div className="social">
+            <FaLinkedin
+              size={20}
+              style={{ color: "#fff", marginRight: "2rem" }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
