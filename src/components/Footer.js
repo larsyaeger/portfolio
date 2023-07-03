a552fa7640e8ccc13dc3aef473588a97ec169324
larsyaeger <larsyaeger@gmail.com>
2023-07-03 06:57:27 -0700

    some minor stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,8 @@ const Footer = () => {
           <div className="location">
             <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
             <div>
-              <p>address goes here</p>
-              <p>city goes here</p>
+              <p>Victoria, British Columbia</p>
+              <p>Canada</p>
             </div>
           </div>
           <div className="phone">
@@ -22,7 +22,7 @@ const Footer = () => {
                 size={20}
                 style={{ color: "#fff", marginRight: "2rem" }}
               />
-              123456789
+              204-806-6263
             </h4>
           </div>
           <div className="email">
@@ -36,14 +36,14 @@ const Footer = () => {
           </div>
         </div>
         <div className="right">
-          <h4>About the company</h4>
+          <h4>About</h4>
           <p>
             ladsfasfsadf adsfg kadfsjg kldfjs gkl jaersig j9aerigju 89aw4fg
             89ahjf jkasj fklas kfjhas jkfajksf
           </p>
           <div className="social">
             <FaLinkedin
-              size={20}
+              size={40}
               style={{ color: "#fff", marginRight: "2rem" }}
             />
           </div>
