c0ac28a7b43bb04b3ff045f5633d4d87663a633d
larsyaeger <larsyaeger@gmail.com>
2023-07-03 17:32:38 -0700

    import and return workcard

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,12 +3,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from "../components/HeroImg2";
+import WorkCard from "../components/WorkCard";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
-      <HeroImg2 heading="This is heading" text="this is text" />
+      <HeroImg2 heading="This is heading for PROJECTS" text="this is text" />
+      <WorkCard />
       <Footer />
     </div>
   );
